77fa444a0b031ecc429e682babdf445904864f50
DYPAK <dypakin@gmail.com>
2024-08-27 23:55:53 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(res => res.AddService("ad"))
+    .WithTracing(tracing => 
+        tracing
+        .AddSource("1 kryg")
+        .AddAspNetCoreInstrumentation(opt => opt.RecordException = true)
+        .AddOtlpExporter());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
