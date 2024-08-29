a81f961bf8c0ff0fe2a7037c022cd58c7751c28d
DYPAK <dypakin@gmail.com>
2024-08-30 01:15:21 +0400

    docker compose

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using OpenTelemetry.Context.Propagation;
+using OpenTelemetry;
 using System.Diagnostics;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace WebApplicationTest.Controllers
 {
@@ -13,10 +17,17 @@ namespace WebApplicationTest.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+        private readonly TextMapPropagator _propagator = Propagators.DefaultTextMapPropagator;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger,
+            IHttpClientFactory httpClientFactory,
+            IConfiguration configuration)
         {
             _logger = logger;
+            _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
@@ -26,14 +37,52 @@ namespace WebApplicationTest.Controllers
 
             activity?.AddTag("Weather", 265);
 
+            Console.WriteLine($"{activity.TraceId} : {activity.SpanId}");
+
+            using (var activity2 = activity.Source.StartActivity("GetWeatherForecast"))
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine($"{activity2.TraceId} : {activity2.SpanId}");
+            }
+
 
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                //Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendToTheOtherApi([FromBody] WeatherForecast weatherForecast)
+        {
+            _logger.LogInformation("Traceparent: {0}", Activity.Current.Id);
+            _logger.LogInformation("Tracestate: {0}", Activity.Current.TraceStateString);
+            var client = _httpClientFactory.CreateClient();
+            var content = new StringContent(JsonConvert.SerializeObject(weatherForecast), Encoding.UTF8, "application/json");
+            await client.PostAsync(_configuration["ClientUrl"], content);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("PublishInQueue")]
+        public IActionResult PublishInQueue([FromBody] WeatherForecast weatherForecast)
+        {
+            var message = JsonConvert.SerializeObject(weatherForecast);
+            var body = Encoding.UTF8.GetBytes(message);
+            var traceparent = Activity.Current.Id;
+            var tracestate = Activity.Current.TraceStateString;
+            _logger.LogInformation("Traceparent: {0}", traceparent);
+            _logger.LogInformation("Tracestate: {0}", tracestate);
+
+            // Inject the ActivityContext into the message headers to propagate trace context to the receiving service.
+            var contextToInject = Activity.Current.Context;
+            _propagator.Inject(new PropagationContext(contextToInject, Baggage.Current), Request.Headers, (headers, key, value) => headers.Append(key, value));
+
+            return Ok();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,14 +9,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
 
-builder.Services.AddOpenTelemetry()
-    .ConfigureResource(res => res.AddService("ad"))
+builder.Services
+    .AddOpenTelemetry()
+    .ConfigureResource(res => res.AddService(builder.Configuration["Jaeger:ServiceName"]))
     .WithTracing(tracing => 
         tracing
-        .AddSource("1 kryg")
+        .AddSource(builder.Configuration["Jaeger:ServiceName"])
         .AddAspNetCoreInstrumentation(opt => opt.RecordException = true)
-        .AddOtlpExporter());
+        .AddConsoleExporter()
+        .AddOtlpExporter(opt => opt.Endpoint = new Uri("http://0.0.0.0:4317"))
+    );
 
 var app = builder.Build();
 
